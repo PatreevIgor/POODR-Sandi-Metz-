42e56c6202328d24825ddeba89569c3f258136e0
Patreev Igor <Patreewigor@yandex.ru>
2019-01-23 12:21:29 +0300

    Update Chapter 6. Inheritance.rb

--- Chapter 6. Inheritance.rb
+++ Chapter 6. Inheritance.rb
@@ -1,1 +1,19 @@
+# Inheritance initialize args
 
+class Bicycle
+  attr_reader :size 
+  def initialize(args={})
+    @size = args[:size] # <- перемещено из RoadBike
+  end
+end
+
+class RoadBike < Bicycle
+  attr_reader :tape_color
+  def initialize(args)
+    @tape_color = args[:tape_color]
+    super(args) # <- RoadBike теперь ДОЛЖЕН отправлять 'super'
+  end
+end
+
+road_bike = RoadBike.new(size: 'M', tape_color: 'red')
+road_bike.size # -> 'M'
